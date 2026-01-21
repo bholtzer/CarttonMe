7fbe84ade6e962753bae9ff8679d1c6b920dd380
bholtzer <61281993+bholtzer@users.noreply.github.com>
2026-01-21 10:15:33 +0200

    Refresh SmurfMe screen layout and copy

---
+++ app/src/main/java/com/carttonme/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.carttonme.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SmurfBlue = Color(0xFF3F51B5)
+val SmurfBlueDark = Color(0xFF283593)
+val SmurfGold = Color(0xFFFFC107)
+val SmurfGray = Color(0xFFEEF0F3)
